5761b8a1fc5e632b1b013cf95207055a42105d73
JustynaWK <owcolot@gmail.com>
2023-09-07 18:05:30 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+Console.WriteLine($"Days to end of subscription: {daysUntilExpiration}");
+
+if (daysUntilExpiration >= 6 && daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+else if (daysUntilExpiration >1 && daysUntilExpiration <= 5)
+{
+    Console.WriteLine($@"Your subscription expires in {daysUntilExpiration} days.");
+    Console.WriteLine("Renew now and save 10%!");
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine(@"Your subscription expires within a day!");
+    Console.WriteLine("Renew now and save 20%!");
+}
+else if (daysUntilExpiration == 0)
+{
+    Console.WriteLine("Your subscription has expired");
+}
+else
+{
+    Console.WriteLine();
+} 
